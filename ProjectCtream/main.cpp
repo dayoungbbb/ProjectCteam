eef24438ffc73203b186fcf629f1bc9788a9e599
MJ <hotgdfjk11@gmail.com>
2022-03-02 15:08:49 +0900

    자료구조 추가

--- ProjectCtream/main.cpp
+++ ProjectCtream/main.cpp
@@ -1,3 +1,18 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Employee {
+    int employeeNum;
+    string name;
+    int cl;
+    string phoneNum;
+    string birthday;
+    int certi;
+};
+
+vector<Employee> dataBase;
+
 int main()
 {
 	return 0;
