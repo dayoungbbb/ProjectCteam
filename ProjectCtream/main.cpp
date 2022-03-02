9804a6c5307b26b1cf2b8cb623b1c7189811f1ec
MJ <hotgdfjk11@gmail.com>
2022-03-02 15:34:17 +0900

    코드리뷰 반영했습니다

--- ProjectCtream/main.cpp
+++ ProjectCtream/main.cpp
@@ -2,18 +2,34 @@
 #include <string>
 using namespace std;
 
+enum CareerLevel {
+    CL1 = 0,
+    CL2 = 1,
+    CL3 = 2,
+    CL4 = 3
+};
+
+enum CertiLevel {
+    ADV = 0,
+    PRO = 1,
+    EXP = 2
+};
+
 struct Employee {
-    int employeeNum;
-    string name;
-    int cl;
+    unsigned int employeeNum;
+    string firstName;
+    string lastName;
+    CareerLevel cl;
     string phoneNum;
-    string birthday;
-    int certi;
+    unsigned int yearOfBday;
+    unsigned int monthOfBday;
+    unsigned int dateOfBday;
+    CertiLevel certi;
 };
 
 vector<Employee> dataBase;
 
 int main()
 {
-	return 0;
+    return 0;
 }
