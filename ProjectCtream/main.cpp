2026a24dc5f748fe8a11a46174902df859bd052f
dayoungbbb <dayoungbbb@gmail.com>
2022-03-03 16:49:27 +0900

    Seperate header from Main
    Seperate Employee header from Main

--- ProjectCtream/main.cpp
+++ ProjectCtream/main.cpp
@@ -1,25 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include "employee.h"
-using namespace std;
-
-
 
 int main()
 {
-    /*  SCH, -p, , , birthday, 19810630           SCH, 07843022, SEO KFI, CL3, 010 - 4837 - 6716, 19810630, ADV
-                                                  SCH, 18050301, KIM EOE, CL4, 010 - 6848 - 4988, 19810630, EX
-        SCH, , , , birthday, 19810630             SCH, 2
-        SCH, , , , employeeNum, 23040123          SCH, NON*/
-    string input = "SCH, , , ,name,KYUMOK KIM";
-    vector<string> v;
-    //vector<string> v = {"SCH", "","" ,"" ,"name", "KYUMOK KIM"};
-    v = split(input, ',' );
-    for (auto va : v) {
-        remove(va.begin(), va.end(), ' ');
-    }
-    search(v);
+
     return 0;
 }
